19f48cbee1794770a55c1f784c4569687e675185
btamila <btamila@pdinet.com>
2021-01-19 17:52:53 +0530

    20210119-1

--- app/routes/main.routes.js
+++ app/routes/main.routes.js
@@ -2,10 +2,10 @@ module.exports = app => {
   const postRouter = require("../controllers/MainPost.controller.js");
   // const getRouter = require("../controllers/MainGet.controller.js");
   
-  let routeBasePath = "/attachment/v1";
+  //let routeBasePath = "/attachment/v1";
   
   // Create a Post Request
-  app.post(routeBasePath+"/HtmlToImage", postRouter.HtmlToImage); 
+  app.post("/HtmlToImage", postRouter.HtmlToImage); 
   
  
   // app.post(routeBasePath+"/HtmlToPdf", postRouter.HtmlToPdf);
